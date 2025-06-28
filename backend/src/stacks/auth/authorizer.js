d660d56554e2a34d1a8110c3e7f8e26146fe0842
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2025-06-27 22:09:54 -0300

    🚧 Testing CD

--- backend/src/stacks/auth/authorizer.js
+++ backend/src/stacks/auth/authorizer.js
@@ -1,8 +1,6 @@
 const { logger, jwt, response } = require("/opt/utils");
 const authorizerHandler = require("./handlers/authorizerHandler");
 
-// Teste remover depois
-
 exports.handler = async (event) => {
   return await authorizerHandler(
     jwt,
--- backend/src/stacks/tenants/handlers/getHandler.js
+++ backend/src/stacks/tenants/handlers/getHandler.js
@@ -4,8 +4,6 @@ const getHandler = async (dynamoDB, logger, response, tableName) => {
   try {
     const tenants = await dynamoDB.scanVanilla(tableName);
 
-    console.debug("teste 123");
-
     return response.success(tenants);
   } catch (error) {
     logger.error("Error listing tenants", {
