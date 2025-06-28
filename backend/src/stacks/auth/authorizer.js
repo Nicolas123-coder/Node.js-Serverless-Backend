1b120081c7d97c544c6de098d7bdfdb558827b48
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2025-06-27 22:06:05 -0300

    🚧 Testing changes detector CD

--- backend/src/stacks/auth/authorizer.js
+++ backend/src/stacks/auth/authorizer.js
@@ -1,6 +1,8 @@
 const { logger, jwt, response } = require("/opt/utils");
 const authorizerHandler = require("./handlers/authorizerHandler");
 
+// Teste remover depois
+
 exports.handler = async (event) => {
   return await authorizerHandler(
     jwt,
